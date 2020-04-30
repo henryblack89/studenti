f344c73fb2e0561c401204b449cc85af3fe5aa94
henryblack89 <64595421+henryblack89@users.noreply.github.com>
2020-04-30 17:33:25 +0200

    Add files via upload

---
+++ studenti-test.sql
@@ -0,0 +1,31 @@
+
+/* Vinvoli referenziali    */        
+
+UPDATE studente
+
+SET matricolaStudente = NULL;
+
+UPDATE studente
+
+SET corsoLaurea = NULL;
+
+UPDATE studente
+
+SET nome = NULL;
+
+UPDATE studente
+
+SET codiceFiscale = 111222333;
+
+
+
+ALTER TABLE studente
+
+DROP COLUMN codiceSede;
+
+
+
+
+
+
+
